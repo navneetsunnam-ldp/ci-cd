923c6e09f4ecbcdc105bc2ec107f6d25a7af0fa0
Navneet Kumar Sunnam_ldp <navneet.sunnam@zemosolabs.com>
2026-03-11 17:56:19 +0530

    demo

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"message": "NEW VERSION DEPLOYED"}
+    return {"message": "This is a demonstration"}
 
 @app.route("/api")
 def api():
