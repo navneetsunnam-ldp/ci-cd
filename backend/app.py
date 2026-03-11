2ee0290f351758354d8e044312b5dc5ca16db413
Navneet Kumar Sunnam_ldp <navneet.sunnam@zemosolabs.com>
2026-03-11 17:33:24 +0530

    v1 error solved

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"message": "CI/CD pipeline Backend Version v1 deployed"}
+    return {"message": "CI/CD pipeline Backend Version v1 error solved"}
 
 @app.route("/api")
 def api():
