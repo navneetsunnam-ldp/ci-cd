5451de05269bccdf39b2b27c904dc7f12cf7ff78
Navneet Kumar Sunnam_ldp <navneet.sunnam@zemosolabs.com>
2026-03-11 18:02:24 +0530

    updated automatic message change

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"message": "This is a demonstration"}
+    return {"message": "This is CI/CD Demo"}
 
 @app.route("/api")
 def api():
